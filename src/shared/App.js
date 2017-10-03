bf849c8f88913087fcbe96a02d48ffbdc5bfba84
Syn-Ee Wong <synee.wong@ie.com.au>
2017-10-03 15:50:53 +1100

    Yay it works! It’s all from App.js -> Troublemaker.

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,21 +1,81 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+import {
+  Route,
+  Switch,
+  Link
+} from 'react-router-dom';
+
+import logo from './logo.svg';
+
+const Header = () => (
+  <div className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <h2>Welcome to Universal React</h2>
+  </div>
+)
+
+export const PageNotFound = (props, context = {}) => {
+  if (context.setStatus) {
+    context.setStatus(404)
   }
+  
+  return (
+    <div>
+      <h1>
+        Page not found
+      </h1>
+      <Link to="/">
+        Go home
+      </Link>
+    </div>
+  )
+}
+PageNotFound.contextTypes = {
+  setStatus: PropTypes.func.isRequired
 }
 
-export default App;
+const TestRouterPage = ({ match }) => (
+  <div className="App-intro">
+    <p>
+      Test page {match.params.id}
+    </p>
+    <p>
+    <Link to={`/`}>
+      Home
+    </Link>
+    </p>
+    <p>
+      <Link to={`/aljlskaklksdkfaj falsflasd`}>
+        Go to non-existent page
+      </Link>
+    </p>
+  </div>
+)
+
+const Home = () => (
+  <div className="App-intro">
+    <p>To get started, edit <code>src/shared/App.js</code> and save to reload.</p>
+    <Link to={`/test/123`}>
+      Test the router
+    </Link>
+  </div>
+)
+
+const App = () => (
+  <div className="App">
+    <Route path="/" component={ ({ match }) => (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/test/:id" component={TestRouterPage}/>
+          <Route component={PageNotFound}/>
+        </Switch>
+      </div>
+    )}/>
+  </div>
+)
+
+export default App
